b8c485ab31de521f2a5dc8e1ef0155b39f8dbfd5
Arnol Fontalvo Fruto <arnol.fontalvo@koombea.com>
2018-08-06 10:23:03 -0500

    Linking Between Routes

--- src/app.js
+++ src/app.js
@@ -1,29 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route} from 'react-router-dom';
+import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 
-const ExpenseDashboardPage = () => (
-  <div>
-    This is from my dashboard component
-  </div>  
-);
-
-const ExpenseDashboardPage2 = () => (
-  <div>
-    Zagy tu nena
-  </div>  
-);
-
-const routes = (
-  <BrowserRouter>
-    <div>
-      <Route path="/" component={ExpenseDashboardPage}/>
-      <Route path="/zagy" component={ExpenseDashboardPage2}/>
-    </div>
-  </BrowserRouter>
-);
 
 // stateless functional component
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
