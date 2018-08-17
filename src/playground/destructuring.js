41cb9786e5d6e3b174542c13a0bd4ef50bae475f
Arnol Fontalvo Fruto <aff@MacBook-Pro-de-Arnol.local>
2018-08-17 07:49:42 -0500

    * ES6 Array Destructuring - Section 10, Lecture 89 * Refactoring and Organizing - Section 10, Lecture 90

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,3 +1,10 @@
+// const add = ({ a, b}, c) => {
+//   return a + b +c;
+// }
+
+// console.log(add({ a: 1, b: 12 }, 100));
+
+
 // const person = {
 //   name: 'Arnol',
 //   age: 37,
@@ -16,14 +23,27 @@
 //   console.log(`It's ${temperature} in ${city}`)
 // }
 
-const book = {
-  title: 'Ego is the Enemy',
-  author: 'Ryan Hiliday',
-  publisher: {
-    name: undefined
-  }
-};
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author: 'Ryan Hiliday',
+//   publisher: {
+//     name: undefined
+//   }
+// };
 
-const { name: publisherName = 'Self-Publisher' } = book.publisher;
+// const { name: publisherName = 'Self-Publisher' } = book.publisher;
 
-console.log(publisherName);
+// console.log(publisherName);
+
+
+//Array destructuring
+
+// const address = ['Calle 53#52-68', 'Barranquilla', 'Atlantico', '0180010']
+
+// const [, city, state] = address
+
+// console.log(`You are in ${city} ${state}.`)
+
+const item = ['Cofee (iced)','$3.00','$3.50','$3.75'];
+const [itemName, ,mediumPrice] = item;
+console.log(`A medium ${itemName} costs ${mediumPrice}.`);
