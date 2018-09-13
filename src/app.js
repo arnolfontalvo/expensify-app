a7ab21c84ec8a3864eac74a1c3f0c1f0be86caa0
Arnol Fontalvo Fruto <arnol.fontalvo@koombea.com>
2018-09-13 09:16:11 -0500

    Connecting Store and Component with React-Redux - Section 11, Lecture 101

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense } from './actions/expenses';
@@ -11,19 +12,22 @@ import './styles/style.scss';
 
 const store = configureStore();
 
-// store.subscribe(() => {
-//   const state = store.getState();
-//   const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-//   console.log(visibleExpenses);
-// });
-
 store.dispatch(addExpense({ description: 'Water bill'}));
 store.dispatch(addExpense({ description: 'Gas bill'}));
-store.dispatch(setTextFilter('bill'));
+store.dispatch(setTextFilter('water'));
+
+setTimeout(() => {
+  store.dispatch(setTextFilter('rent'));
+}, 3000);
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
 console.log(visibleExpenses);
 
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
 // stateless functional component
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+ReactDOM.render(jsx, document.getElementById('app'));
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,24 @@
+/*jslint node: true */
+/*jshint -W079 */
+'use strict';
+
+import React from 'react';
+import { connect } from 'react-redux';
+import filters from '../reducers/filters'
+
+const ExpenseList = (props) => (
+    <div>
+        <h1>Expense List</h1>
+        {props.filters.text} - 
+        {props.expenses.length}
+    </div>
+);
+
+const mapStateToProps = (state) => {
+  return {
+    expenses: state.expenses,
+    filters: state.filters
+  };
+};
+
+export default connect(mapStateToProps)(ExpenseList);
