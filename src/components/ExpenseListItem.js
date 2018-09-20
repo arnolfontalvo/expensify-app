d1a00c44cbaa13b5acc88cced5f3647953fc1c0c
Arnol Fontalvo Fruto <aff@MacBook-Pro-de-Arnol.local>
2018-09-20 07:35:33 -0500

    Controlled Inputs for Filters - Section 11, Lecture 103

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -3,12 +3,17 @@
 'use strict';
 
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
-const ExpenseListItem = ({ description, amount, createAt }) => (
+const ExpenseListItem = ({ dispatch, id, description, amount, createAt }) => (
   <div>
     <h3>{description}</h3>
     <p>{amount} - {createAt}</p>
+    <button onClick={(e) => {
+      dispatch(removeExpense({ id }));
+    }}>Remove</button>
   </div>
 );
 
-export default ExpenseListItem;
+export default connect()(ExpenseListItem);
