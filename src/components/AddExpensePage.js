caf73e5cb18d481a506c08ae7e52e03380283f50
Arnol Fontalvo Fruto <aff@MacBook-Pro-de-Arnol.local>
2018-09-22 08:45:19 -0500

    Creating Expense Add/Edit Form - Section 11, Lecture 105

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -3,10 +3,11 @@
 'use strict';
 
 import React from 'react';
+import ExpenseForm from './ExpenseForm';
 
 const AddExpensePage = () => (
   <div>
-    This is from my add expense component
+    <ExpenseForm />
   </div>
 );
 
