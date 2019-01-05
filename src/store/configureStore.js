8589dac98b6c93e52751bbd536b146af36d0a193
Arnol Fontalvo Fruto <aff@MacBook-Pro-de-Arnol.local>
2019-01-05 16:16:00 -0500

    Adds redux developer tool

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -12,15 +12,9 @@ export default () => {
     combineReducers({
       expenses: expensesReducer,
       filters: filtersReducer
-    })
+    }),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
 
   return store;
 };
-// Store creation 
-const store = createStore(
-  combineReducers({
-    expenses: expensesReducer,
-    filters: filtersReducer
-  })
-);
